fc770afd735a1f3de94849dbfb777484a1866e9b
Hyejin Seok <hyejin.seok7@gmail.com>
2025-05-27 20:16:57 -0700

    Update UI

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # generosity-x-trial
+
+- Build an AB testing using VWO
+- Project Name: `WHM // CRO - Returning Visitor Monthly Gift Ask CRO Test`
--- index.js
+++ index.js
@@ -1,32 +1,49 @@
-document.querySelector("#secondary_cta_two_items_1").insertAdjacentHTML(
-  "afterend",
-  `
-<div class="secondary-cta-two-items"><section id="monthly-donor-banner" class="monthly-donor-banner" style="background: #b0e4f1;">
-  <div class="container">
-    <div class="justify-content-center row row-eq-height">
-      <div class="col wrapper">
-        <div class="row darkgray-text justify-content-center">
-          <div class="col-md-12 col-10  content-wrapper text-center">
-            <h2 class="headline">The Incredible Impact of Monthly Giving:</h2>
-            <ul class="monthly-list text-main">
-              <li>Create a stable foundation so we can focus on long-term solutions</li>
-              <li>Reduce our fundraising costs, directing more resources to our mission</li>
-              <li>Allow us to budget effectively and commit to sustainable programs</li>
-              <li>Make a difference that never stops, 365 days a year</li>
+(function () {
+  const isMarkupInjected = document.querySelectorAll(
+    "#monthly-donor-banner"
+  ).length;
+  const markup = `
+    <section id="monthly-donor-banner" class="monthly-donor-banner">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-12 col-10 content-wrapper">
+            <h2 class="headline text-center">
+              The Incredible Impact of Monthly&nbsp;Giving:
+            </h2>
+            <ul class="monthly-list text-sm-center">
+              <li>
+                Create a stable foundation so we can focus on
+                long-term&nbsp;solutions
+              </li>
+              <li>
+                Reduce our fundraising costs, directing more resources to
+                our&nbsp;mission
+              </li>
+              <li>
+                Allow us to budget effectively and commit to
+                sustainable&nbsp;programs
+              </li>
+              <li>Make a difference that never stops, 365 days a&nbsp;year</li>
             </ul>
-            <div class="col-12 col-md-auto btn-wrapper centre">
-              <a href="/ways-to-give/shelter-food-love/"
-                 class="btn btn-normal btn-monthly"
-                 data-feathr-click-track="true"
-                 data-feathr-link-aids="6680097d182d4e7b419ab7b5">
-                 Become a Monthly Donor
-              </a>
+            <div class="btn-wrapper text-center">
+              <a
+                href="/ways-to-give/shelter-food-love/"
+                class="btn btn-normal btn-monthly"
+                data-feathr-click-track="true"
+                data-feathr-link-aids="6680097d182d4e7b419ab7b5"
+              >
+                Become a Monthly Donor
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
-`
-);
+      </section>
+    `;
+
+  // Check if the markup is already injected
+  !isMarkupInjected &&
+    document
+      .querySelector("#secondary_cta_two_items_1")
+      .insertAdjacentHTML("afterend", markup);
+})();
