d402dc1c6820b94cd50cbeb1464189059a459c6b
Hyejin Seok <hyejin.seok7@gmail.com>
2025-05-27 17:28:44 -0700

    First init

---
+++ index.js
@@ -0,0 +1,32 @@
+document.querySelector("#secondary_cta_two_items_1").insertAdjacentHTML(
+  "afterend",
+  `
+<div class="secondary-cta-two-items"><section id="monthly-donor-banner" class="monthly-donor-banner" style="background: #b0e4f1;">
+  <div class="container">
+    <div class="justify-content-center row row-eq-height">
+      <div class="col wrapper">
+        <div class="row darkgray-text justify-content-center">
+          <div class="col-md-12 col-10  content-wrapper text-center">
+            <h2 class="headline">The Incredible Impact of Monthly Giving:</h2>
+            <ul class="monthly-list text-main">
+              <li>Create a stable foundation so we can focus on long-term solutions</li>
+              <li>Reduce our fundraising costs, directing more resources to our mission</li>
+              <li>Allow us to budget effectively and commit to sustainable programs</li>
+              <li>Make a difference that never stops, 365 days a year</li>
+            </ul>
+            <div class="col-12 col-md-auto btn-wrapper centre">
+              <a href="/ways-to-give/shelter-food-love/"
+                 class="btn btn-normal btn-monthly"
+                 data-feathr-click-track="true"
+                 data-feathr-link-aids="6680097d182d4e7b419ab7b5">
+                 Become a Monthly Donor
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+`
+);
